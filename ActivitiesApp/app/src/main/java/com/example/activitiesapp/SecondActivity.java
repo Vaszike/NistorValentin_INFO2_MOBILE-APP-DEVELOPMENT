b53ab7bb550b302011a6925a97198fe4e3beef18
Vaszike <138787895+Vaszike@users.noreply.github.com>
2025-03-12 00:21:07 +0200

    ActivitiesApp adaugat

---
+++ ActivitiesApp/app/src/main/java/com/example/activitiesapp/SecondActivity.java
@@ -0,0 +1,33 @@
+package com.example.activitiesapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Button btnGoToThird = findViewById(R.id.btnGoToThird);
+        btnGoToThird.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btnGoBack = findViewById(R.id.btnGoBack);
+        btnGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, FirstActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
